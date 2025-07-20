3ef24ce0d50e2af940774120a12f1a516df78adb
rahul6965 <8467rahulsrivastav@gmail.com>
2025-07-20 17:29:39 +0530

    change hii

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
           <Routes>
             <Route path='/' element={<Layout />}>
-              <Route index element={<Home />} />
+              <Route index element={<Addcard />} />
               <Route path='/about' element={<About />} />
               <Route path='/creat-post' element={<Addcard />} />
               <Route path='/card-page' element={<Cardpage />} />
